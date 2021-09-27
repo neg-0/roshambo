b2245896fa5dbc986d073b53dcb1beec9ff0cbae
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-09-27 12:39:39 -0400

    Added logic to check computer move and gameplay

--- Main.js
+++ Main.js
@@ -7,10 +7,36 @@ const Move = require("./Move")
 
 console.log("Playing a game of Roshambo against the computer.")
 
-const move = Move.getMove(argv.move)
+// Creats a new Move object based on the player's input
+const playerMove = Move.GetMove(argv.move)
 
-if (move) {
-    console.log(`Player plays ${move.name}`)
+// Checks to see if Move is valid and displays the output
+if (playerMove) {
+    console.log(`Player plays ${playerMove.name}!`)
 } else {
     console.log("Player has made an invalid move!")
+    return
+}
+
+// Creates a random computer move
+const computerMove = Move.GetRandomMove()
+
+console.log(`Computer plays ${computerMove.name}!`)
+
+// Evaluate the two moves
+const result = playerMove.beatsMove(computerMove)
+
+switch (result) {
+    case "win": {
+        console.log("~The Player Wins!~")
+        break;
+    }
+    case "lose": {
+        console.log("~The Computer Wins!~")
+        break;
+    }
+    case "draw": {
+        console.log("~It's a Draw!~")
+        break;
+    }
 }
